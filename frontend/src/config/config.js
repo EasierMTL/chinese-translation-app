ece6fface7cbdbf5b72ab0d02500abb3e6c3b199
Joseph Chen <jchen42703@gmail.com>
2022-04-15 16:38:52 -0400

    Deployment docs and config fix

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,4 +1,6 @@
 export const config = {
   baseUrl:
-    process.env.NODE_ENV === "production" ? "N/A" : "http://127.0.0.1:8000",
+    process.env.NODE_ENV === "production"
+      ? "https://chinesetranslationapi.com/"
+      : "http://127.0.0.1:5001",
 };
