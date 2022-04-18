ce7d51d67b7117ec5a0092c7a2ba221872c6cbbf
Joseph Chen <jchen42703@gmail.com>
2022-04-18 02:37:18 -0400

    new docker commands

--- server/aws/deploy_docker.sh
+++ server/aws/deploy_docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git clone https://github.com/jchen42703/chinese-translation-api.git chinese
-cd ~/chinese
-docker-compose up
+cd ~/chinese/server
+docker build -t chinese_translation_server .
+docker run -e ENV_TYPE="production" -e DEPLOY_TYPE="server" --name translation_server_container_deploy -p 80:5001 chinese_translation_server
