421440625fa31f696300cf8d065963684e13fa15
Joseph Chen <jchen42703@gmail.com>
2022-04-24 15:20:28 -0400

    made creating the translator router into a function

--- server/api/routes/translate.py
+++ server/api/routes/translate.py
@@ -1,4 +1,4 @@
-"""The main endpoint that will be tested
+"""The main endpoint(s) that will be tested
 """
 
 from starlette.responses import JSONResponse
@@ -7,50 +7,46 @@ from fastapi import APIRouter
 import os
 from api.models.base import ChineseToEnglishTranslator, EnglishToChineseTranslator
 
-router = APIRouter()
 
+def create_translator_router(deploy_type: str = "server"):
+    router = APIRouter()
 
-class ChineseTextToTranslateReq(BaseModel):
-    """Chinese to english  translation request body
-    """
-    text: str
-
-    class Config:
-        """example docs"""
-        schema_extra = {"example": {"text": "我爱ECSE484"}}
-
-
-zh_translator = ChineseToEnglishTranslator()
-
-
-@router.post("/translate/chinese")
-async def run_prediction(req: ChineseTextToTranslateReq) -> JSONResponse:
-    """Runs the model prediction
-    """
-    print(req)
-    prediction = zh_translator.predict(req.text)
-    return {'prediction': prediction}
-
-
-deploy_type = os.environ.get("DEPLOY_TYPE")
-
-if deploy_type != "server":
-
-    class EnglishToChineseTranslateReq(BaseModel):
-        """English to chinese translation JSON body
+    class ChineseTextToTranslateReq(BaseModel):
+        """Chinese to english  translation request body
         """
         text: str
 
         class Config:
             """example docs"""
-            schema_extra = {"example": {"text": "I love ECSE 484"}}
+            schema_extra = {"example": {"text": "我爱ECSE484"}}
 
-    en_translator = EnglishToChineseTranslator()
+    zh_translator = ChineseToEnglishTranslator()
 
-    @router.post("/translate/english")
-    async def run_english_to_chinese_pred(
-            req: EnglishToChineseTranslateReq) -> JSONResponse:
+    @router.post("/translate/chinese")
+    async def run_prediction(req: ChineseTextToTranslateReq) -> JSONResponse:
         """Runs the model prediction
         """
-        prediction = en_translator.predict(req.text)
+        print(req)
+        prediction = zh_translator.predict(req.text)
         return {'prediction': prediction}
+
+    if deploy_type != "server":
+
+        class EnglishToChineseTranslateReq(BaseModel):
+            """English to chinese translation JSON body
+            """
+            text: str
+
+            class Config:
+                """example docs"""
+                schema_extra = {"example": {"text": "I love ECSE 484"}}
+
+        en_translator = EnglishToChineseTranslator()
+
+        @router.post("/translate/english")
+        async def run_english_to_chinese_pred(
+                req: EnglishToChineseTranslateReq) -> JSONResponse:
+            """Runs the model prediction
+            """
+            prediction = en_translator.predict(req.text)
+            return {'prediction': prediction}
