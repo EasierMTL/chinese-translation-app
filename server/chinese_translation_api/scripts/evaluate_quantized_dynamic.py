68351f7c3f00e1ad137dd7cacef72916de34b360
Joseph Chen <jchen42703@gmail.com>
2022-04-25 11:12:52 -0400

    Quantized model evaluation script

---
+++ server/chinese_translation_api/scripts/evaluate_quantized_dynamic.py
@@ -0,0 +1,26 @@
+import torch
+from chinese_translation_api.models.base import ChineseToEnglishTranslator
+from chinese_translation_api.evaluation.evaluation_pipeline import EvaluationPipeline
+from chinese_translation_api.models.load import download_model, model_params
+
+if __name__ == "__main__":
+    from datetime import datetime, timedelta
+
+    translator = ChineseToEnglishTranslator()
+    translator.model = torch.quantization.quantize_dynamic(translator.model,
+                                                           {torch.nn.Linear},
+                                                           dtype=torch.qint8)
+    pipeline = EvaluationPipeline(predictor=translator)
+    pipeline.load_dset("./test_data/opus-2020-07-17.test.txt")
+
+    now = datetime.now()
+
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time =", current_time)
+    pipeline.evaluate(max_samples=10, num_workers=2)
+
+    after = datetime.now()
+    after_time = after.strftime("%H:%M:%S")
+    print("After Evaluation =", after_time)
+    duration = (after - now).total_seconds()
+    print("Duration: ", str(timedelta(seconds=duration)))
