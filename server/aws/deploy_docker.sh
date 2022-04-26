e1af5b294a3eb0387abecf31c86a31dfaeeea837
Joseph Chen <jchen42703@gmail.com>
2022-04-25 22:08:09 -0400

    new scripts

--- server/aws/deploy_docker.sh
+++ server/aws/deploy_docker.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
+sudo apt update
+# Install docker
+sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt -y install docker-ce
+
 git clone https://github.com/jchen42703/chinese-translation-api.git chinese
 cd ~/chinese/server
-docker build -t chinese_translation_server .
-docker run -e ENV_TYPE="production" -e DEPLOY_TYPE="server" --name translation_server_container_deploy -p 80:5001 chinese_translation_server
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-20-04
+
+sudo docker build -t chinese_translation_server .
+
+# Regular
+sudo docker run -d -e ENV_TYPE="production" -e DEPLOY_TYPE="server" --name translation_server_container_deploy -p 5001:5001 chinese_translation_server
+
+# Quantized
+# sudo docker run -d -e ENV_TYPE="production" -e DEPLOY_TYPE="server" MODEL_TYPE="quantized_dynamic" --name translation_server_container_deploy -p 5001:5001 chinese_translation_server
