49704ccbe2fecd8bf48e08450cc37b1ca5945130
Joseph Chen <jchen42703@gmail.com>
2022-11-02 02:50:16 -0400

    Cleanup and moving files around

--- deploy/loadtest/locustfile.py
+++ deploy/loadtest/locustfile.py
@@ -3,7 +3,6 @@ import json
 from chinese_translation_api.evaluation.evaluation_pipeline import EvaluationPipeline
 from chinese_translation_api.models.base import ChineseToEnglishTranslator
 from locust import HttpUser, task, between
-import pip
 
 
 def load_dset():
@@ -23,7 +22,7 @@ class TranslateUser(HttpUser):
     wait_time = between(1, 5)
 
     @task
-    def translate_chineseToEng(self):
+    def translate_chinese_to_eng(self):
 
         # chinese_words = ["你好", "爱", "幸福", "微笑", "中国人", "是的", "再见", "谢谢你"]
         req = {"text": random.choice(chinese_dset)}
--- server/chinese_translation_api/models/base.py
+++ server/chinese_translation_api/models/base.py
@@ -5,57 +5,54 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 
 class Predictor:
-
     def predict(self, message: str) -> str:
         raise NotImplementedError()
 
 
 class ChineseToEnglishTranslator(Predictor):
-    """Inference object for chinese to english translation.
-    """
+    """Inference object for chinese to english translation."""
 
     def __init__(self, model_path: str = None):
         # English to Chinese: https://huggingface.co/Helsinki-NLP/opus-mt-en-zh
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            "Helsinki-NLP/opus-mt-zh-en")
+        self.tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-zh-en")
 
-        if (model_path is None):
+        if model_path is None:
             self.model = AutoModelForSeq2SeqLM.from_pretrained(
-                "Helsinki-NLP/opus-mt-zh-en")
+                "Helsinki-NLP/opus-mt-zh-en"
+            )
             print("Loaded default model")
         else:
             self.model = torch.load(model_path)
 
     def predict(self, message):
-        """Runs the prediction pipeline.
-        """
+        """Runs the prediction pipeline."""
         inputs = self.tokenizer(message, return_tensors="pt")
         translated = self.model.generate(**inputs)
         translated_text = self.tokenizer.batch_decode(
-            translated, skip_special_tokens=True)[0]
+            translated, skip_special_tokens=True
+        )[0]
         return translated_text
 
 
 class EnglishToChineseTranslator(Predictor):
-    """English to Chinese Translator
-    """
+    """English to Chinese Translator"""
 
     def __init__(self, model_path: str = None):
         # English to Chinese: https://huggingface.co/Helsinki-NLP/opus-mt-en-zh
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            "Helsinki-NLP/opus-mt-en-zh")
+        self.tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-zh")
 
-        if (model_path == None):
+        if model_path == None:
             self.model = AutoModelForSeq2SeqLM.from_pretrained(
-                "Helsinki-NLP/opus-mt-en-zh")
+                "Helsinki-NLP/opus-mt-en-zh"
+            )
         else:
             self.model = torch.load(model_path)
 
     def predict(self, message):
-        """Runs the prediction pipeline.
-        """
+        """Runs the prediction pipeline."""
         inputs = self.tokenizer(message, return_tensors="pt")
         translated = self.model.generate(**inputs)
         translated_text = self.tokenizer.batch_decode(
-            translated, skip_special_tokens=True)[0]
+            translated, skip_special_tokens=True
+        )[0]
         return translated_text
