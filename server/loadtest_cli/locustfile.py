668a1e7c3bac70a0b85b189a4b60ecfa5e2023a1
Joseph Chen <jchen42703@gmail.com>
2022-11-05 01:54:10 -0400

    Made the dataset parsing path relative to the locustfile

--- server/loadtest_cli/locustfile.py
+++ server/loadtest_cli/locustfile.py
@@ -1,14 +1,19 @@
+from locust import HttpUser, task, between
 import random
 import json
+import os
 from chinese_translation_api.evaluation.evaluation_pipeline import EvaluationPipeline
 from chinese_translation_api.models.base import ChineseToEnglishTranslator
-from locust import HttpUser, task, between
 
 
 def load_dset():
     translator = ChineseToEnglishTranslator()
     pipeline = EvaluationPipeline(predictor=translator)
-    pipeline.load_dset("./test_data/opus-2020-07-17.test.txt")
+
+    file_path = os.path.abspath(__file__)
+    cli_path = os.path.dirname(file_path)
+    dset_path = os.path.join(cli_path, "test_data", "opus-2020-07-17.test.txt")
+    pipeline.load_dset(dset_path)
     global chinese_dset
     chinese_dset = pipeline.test_ch
 
