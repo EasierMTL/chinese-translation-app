1f8307cc166ceac557570a42f38bbeca11f1b679
Joseph Chen <jchen42703@gmail.com>
2022-12-02 18:29:24 -0500

    ui(index): Added proper descriptions for Home

--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -1,6 +1,7 @@
 import ToTranslateTextArea from "./demo/ToTranslateTextArea";
 import "../styles/Home.module.css";
 import Head from "next/head";
+import UnderlinedLink from "../components/UnderlinedLink";
 
 export default function Home() {
   return (
@@ -14,13 +15,32 @@ export default function Home() {
           content="Translating from Chinese to English with AI."
         />
       </Head>
-      <div className="mt-4 mx-8">
-        <h1 className="text-3xl font-bold">Translate English to Chinese</h1>
-        <span>Remember to add a period or it bugs out :p</span>
+      <div className="mt-4 mx-16">
+        <h1 className="text-3xl font-bold">
+          Translation with Deep Learning Models Demo
+        </h1>
+        <p>
+          This is a prototype of how deep learning models perform in production
+          with CPUs. Future plans will be to scale this to a platform to make
+          translating novels with machine translations easier!
+        </p>
+        <p>
+          The UI and API code are open sourced at:{" "}
+          <UnderlinedLink href="https://github.com/EasierMTL/chinese-translation-app">
+            https://github.com/EasierMTL/chinese-translation-app
+          </UnderlinedLink>
+          . The models are open sourced at{" "}
+          <UnderlinedLink href="https://github.com/EasierMTL/asian_mtl">
+            https://github.com/EasierMTL/asian_mtl
+          </UnderlinedLink>
+          .
+        </p>
+        <h1 className="text-2xl font-bold">Translate English to Chinese</h1>
+        <span>Adding a period will make the translations more accurate!</span>
         <br />
         <span>Type below:</span>
         <ToTranslateTextArea inputMode={"en"}></ToTranslateTextArea>
-        <h1 className="text-3xl font-bold">Translate Chinese to English</h1>
+        <h1 className="text-2xl font-bold">Translate Chinese to English</h1>
         <span>Type below:</span>
         <ToTranslateTextArea inputMode={"ch"}></ToTranslateTextArea>
       </div>
