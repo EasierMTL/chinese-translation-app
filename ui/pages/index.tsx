ef857071d78be4a00ec4da7d2ea47bdb31389d3d
Joseph Chen <jchen42703@gmail.com>
2022-12-02 16:56:12 -0500

    ui(home): set title for Home

--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -1,9 +1,19 @@
 import ToTranslateTextArea from "./demo/ToTranslateTextArea";
 import "../styles/Home.module.css";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>EasierMTL</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content="Translating from Chinese to English with AI."
+        />
+      </Head>
       <div className="mt-4 mx-8">
         <h1 className="text-3xl font-bold">Translate English to Chinese</h1>
         <span>Remember to add a period or it bugs out :p</span>
