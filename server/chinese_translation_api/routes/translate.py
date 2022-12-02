8a1e80749c98a30964f9603c81a554c9ee0015d7
Joseph Chen <jchen42703@gmail.com>
2022-12-02 01:19:06 -0500

    feat: integrated asian-mtl

--- server/chinese_translation_api/routes/translate.py
+++ server/chinese_translation_api/routes/translate.py
@@ -5,11 +5,10 @@ import os
 from starlette.responses import JSONResponse
 from pydantic import BaseModel
 from fastapi import APIRouter
-from chinese_translation_api.models.base import (
+from asian_mtl.models.base import (
     ChineseToEnglishTranslator,
     EnglishToChineseTranslator,
 )
-from chinese_translation_api.models.load import download_model, model_params
 
 
 def create_translator_router(deploy_type: str = "server",
@@ -26,13 +25,16 @@ def create_translator_router(deploy_type: str = "server",
             schema_extra = {"example": {"text": "我爱ECSE484"}}
 
     router = APIRouter()
-    model_path = None if model_type is None else model_params[model_type][
-        "save_path"]
-    if model_type is not None:
-        # Download model if doesn't exist
-        if not os.path.isfile(model_path):
-            download_model(model_path, model_params[model_type]["file_id"])
+    # model_path = None if model_type is None else model_params[model_type][
+    #     "save_path"]
+    model_path = None
+    # if model_type is not None:
+    # Download model if doesn't exist
+    # if not os.path.isfile(model_path):
+    #     download_model(model_path, model_params[model_type]["file_id"])
     zh_translator = ChineseToEnglishTranslator(model_path)
+    if model_type == "quantized_dynamic":
+        zh_translator.quantize()
 
     @router.post("/translate/chinese")
     async def run_prediction(req: ChineseTextToTranslateReq) -> JSONResponse:
@@ -41,6 +43,8 @@ def create_translator_router(deploy_type: str = "server",
         prediction = zh_translator.predict(req.text)
         return {"prediction": prediction}
 
+    # Only initialize the EnglishToChinese for the demo full stack web app.
+    # If it's for load testing, it's not necessary.
     if deploy_type != "server":
 
         class EnglishToChineseTranslateReq(BaseModel):
@@ -54,6 +58,8 @@ def create_translator_router(deploy_type: str = "server",
                 schema_extra = {"example": {"text": "I love ECSE 484"}}
 
         en_translator = EnglishToChineseTranslator()
+        if model_type == "quantized_dynamic":
+            en_translator.quantize()
 
         @router.post("/translate/english")
         async def run_english_to_chinese_pred(
