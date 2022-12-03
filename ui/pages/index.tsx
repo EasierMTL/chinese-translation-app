ad7fc1dceb2aef3e4605a34c3f6543c9af390e8c
Joseph Chen <jchen42703@gmail.com>
2022-12-03 02:01:51 -0500

    feat(ui): better description for demo

--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -19,12 +19,22 @@ export default function Home() {
         <h1 className="text-3xl font-bold">
           Translation with Deep Learning Models Demo
         </h1>
-        <p>
+        <p className="my-4">
           This is a prototype of how deep learning models perform in production
-          with CPUs. Future plans will be to scale this to a platform to make
-          translating novels with machine translations easier!
+          with CPUs. The model used for this demo are dynamically quantized
+          versions of{" "}
+          <UnderlinedLink href="https://huggingface.co/Helsinki-NLP/opus-mt-zh-en">
+            <code>Helsinki-NLP/opus-mt-zh-en</code>{" "}
+          </UnderlinedLink>{" "}
+          for Chinese to English translation and{" "}
+          <UnderlinedLink href="https://huggingface.co/Helsinki-NLP/opus-mt-en-zh">
+            <code>Helsinki-NLP/opus-mt-en-zh</code>{" "}
+          </UnderlinedLink>{" "}
+          for English to Chinese translation. Future plans will be to change
+          this demo into a platform that makes translating novels with machine
+          translations easier!
         </p>
-        <p>
+        <p className="mb-4">
           The UI and API code are open sourced at:{" "}
           <UnderlinedLink href="https://github.com/EasierMTL/chinese-translation-app">
             https://github.com/EasierMTL/chinese-translation-app
