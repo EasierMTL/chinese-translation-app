034321c1ffbe05a2a0652b585e40f301696d8989
Joseph Chen <jchen42703@gmail.com>
2022-12-03 00:09:38 -0500

    ui: better server error handling with toasts

--- ui/pages/demo/ToTranslateTextArea.tsx
+++ ui/pages/demo/ToTranslateTextArea.tsx
@@ -1,11 +1,10 @@
 import { Editor, EditorState } from "draft-js";
 import { useEffect, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 import "../../styles/Home.module.css";
-import {
-  translateChineseToEnglish,
-  translateEnglishToChinese,
-} from "../../services/translate.service";
+import { translateWithAPI } from "../../services/translate.service";
 import { NoSSR } from "../../components/NoSSR";
+import { HTTPError } from "../../utils/err";
 
 const ToTranslateTextArea = ({ inputMode }: { inputMode: "ch" | "en" }) => {
   const [editorState, setEditorState] = useState(() =>
@@ -17,13 +16,39 @@ const ToTranslateTextArea = ({ inputMode }: { inputMode: "ch" | "en" }) => {
   const translate = async () => {
     let prediction;
     const editorText = editorState.getCurrentContent().getPlainText();
-    if (inputMode === "ch") {
-      prediction = await translateChineseToEnglish(editorText);
-    } else {
-      prediction = await translateEnglishToChinese(editorText);
+    try {
+      prediction = await translateWithAPI(editorText, inputMode);
+      setTranslatedText(prediction);
+    } catch (err: unknown) {
+      let msg: string;
+      if (err instanceof HTTPError) {
+        // Raise error toast
+        switch (err.status) {
+          case 429:
+            msg =
+              "Sent too many translations. Please wait a couple seconds before translating again!";
+            break;
+          case 400:
+            msg =
+              "Text too long (>512 characters). Please try again with less text.";
+            break;
+          default:
+            msg = err.message;
+            break;
+        }
+      } else if (err instanceof Error) {
+        if (err.message == "Failed to fetch") {
+          msg = "Server down. Please try again later.";
+        } else {
+          msg = err.message;
+        }
+      } else {
+        msg = `Unknown error translating occurred. Please try again later.`;
+      }
+      toast.error(msg, {
+        duration: 15000,
+      });
     }
-
-    setTranslatedText(prediction);
   };
 
   useEffect(() => {
@@ -37,6 +62,9 @@ const ToTranslateTextArea = ({ inputMode }: { inputMode: "ch" | "en" }) => {
 
   return (
     <div>
+      <div>
+        <Toaster />
+      </div>
       <NoSSR>
         <Editor editorState={editorState} onChange={setEditorState} />
         <span>Translated Text: {translatedText}</span>
